d79655e3ff2cc268db4c3bf69943e6ccaf6f2fbb
i-safronov <ilya.safa1337w@gmail.com>
2024-10-02 21:08:59 +0500

    Added NumberRepository.kt

---
+++ app/src/main/java/com/safronov/apex_udf/example/interesting_fact_about_number/domain/repository/NumberRepository.kt
@@ -0,0 +1,13 @@
+package com.safronov.apex_udf.example.interesting_fact_about_number.domain.repository
+
+import com.safronov.apex_udf.example.interesting_fact_about_number.domain.model.fact.input.GetFactAboutNumberInput
+import com.safronov.apex_udf.example.interesting_fact_about_number.domain.model.fact.output.FactAboutNumber
+import com.safronov.apex_udf.example.interesting_fact_about_number.domain.response.Response
+import kotlinx.coroutines.flow.Flow
+
+interface NumberRepository {
+
+    fun getFactAboutNumber(getFactAboutNumberInput: GetFactAboutNumberInput): Flow<Response<FactAboutNumber>>
+    fun getFactAboutRandomNumber(): Flow<Response<FactAboutNumber>>
+
+}
