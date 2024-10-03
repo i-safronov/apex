969d213018e8b022b48cfd164bfcb0aa82796f9c
i-safronov <ilya.safa1337w@gmail.com>
2024-10-03 19:27:11 +0500

    Changed FactAboutNumberService.kt and NumberRepositoryImpl.kt

--- app/src/main/java/com/safronov/apex_udf/example/interesting_fact_about_number/data/repository/NumberRepositoryImpl.kt
+++ app/src/main/java/com/safronov/apex_udf/example/interesting_fact_about_number/data/repository/NumberRepositoryImpl.kt
@@ -1,6 +1,31 @@
 package com.safronov.apex_udf.example.interesting_fact_about_number.data.repository
 
-class NumberRepositoryImpl(
+import com.safronov.apex_udf.example.interesting_fact_about_number.data.db.FactAboutNumberDao
+import com.safronov.apex_udf.example.interesting_fact_about_number.data.service.FactAboutNumberService
+import com.safronov.apex_udf.example.interesting_fact_about_number.domain.model.fact.input.GetFactAboutNumberInput
+import com.safronov.apex_udf.example.interesting_fact_about_number.domain.model.fact.output.FactAboutNumber
+import com.safronov.apex_udf.example.interesting_fact_about_number.domain.repository.NumberRepository
+import com.safronov.apex_udf.example.interesting_fact_about_number.domain.response.Response
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+import javax.inject.Singleton
 
-) {
+@Singleton
+class NumberRepositoryImpl @Inject constructor(
+    private val factAboutNumberDao: FactAboutNumberDao,
+    private val factAboutNumberService: FactAboutNumberService
+): NumberRepository {
+
+    override fun getFactAboutNumber(getFactAboutNumberInput: GetFactAboutNumberInput): Flow<Response<FactAboutNumber>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getFactAboutRandomNumber(): Flow<Response<FactAboutNumber>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getCachedFacts(): Flow<Response<List<FactAboutNumber>>> {
+        TODO("Not yet implemented")
+    }
+
 }
--- app/src/main/java/com/safronov/apex_udf/example/interesting_fact_about_number/data/service/FactAboutNumberService.kt
+++ app/src/main/java/com/safronov/apex_udf/example/interesting_fact_about_number/data/service/FactAboutNumberService.kt
@@ -1,11 +1,11 @@
-package com.safronov.apex_udf.example.interesting_fact_about_number.data.dao
+package com.safronov.apex_udf.example.interesting_fact_about_number.data.service
 
 import com.safronov.apex_udf.example.interesting_fact_about_number.data.model.output.FactAboutNumberDTO
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 
-interface NumberDao {
+interface FactAboutNumberService {
 
     @GET("$API/{number}")
     fun getFactAboutNumber(@Path("number") number: Long): Response<FactAboutNumberDTO>
