6a434e1ffa96332ae53e03177a6008d1f9a8503f
i-safronov <ilya.safa1337w@gmail.com>
2024-10-03 21:03:35 +0500

    Configure MainActivity.kt

--- app/src/main/java/com/safronov/apex_udf/example/interesting_fact_about_number/ui/activity/MainActivity.kt
+++ app/src/main/java/com/safronov/apex_udf/example/interesting_fact_about_number/ui/activity/MainActivity.kt
@@ -1,21 +1,20 @@
 package com.safronov.apex_udf.example.interesting_fact_about_number.ui.activity
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.safronov.apex_udf.R
+import androidx.activity.ComponentActivity
+import dagger.hilt.android.AndroidEntryPoint
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContent {
+            MaterialTheme {
+                Text("Hello world!")
+            }
         }
     }
 }
