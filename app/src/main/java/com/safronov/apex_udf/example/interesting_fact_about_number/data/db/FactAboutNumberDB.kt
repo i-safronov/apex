c1af53004ce20ae77da2a0930ec2879fab5a9ce9
i-safronov <ilya.safa1337w@gmail.com>
2024-10-03 19:24:33 +0500

    Changed FactAboutNumberDB.kt

--- app/src/main/java/com/safronov/apex_udf/example/interesting_fact_about_number/data/db/FactAboutNumberDB.kt
+++ app/src/main/java/com/safronov/apex_udf/example/interesting_fact_about_number/data/db/FactAboutNumberDB.kt
@@ -1,4 +1,17 @@
 package com.safronov.apex_udf.example.interesting_fact_about_number.data.db
 
-class FactAboutNumberDB {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.safronov.apex_udf.example.interesting_fact_about_number.data.db.FactAboutNumberDB.Companion.DB_VERSION
+import com.safronov.apex_udf.example.interesting_fact_about_number.data.model.output.FactAboutNumberEntity
+
+@Database(entities = [FactAboutNumberEntity::class], version = DB_VERSION)
+abstract class FactAboutNumberDB: RoomDatabase() {
+
+    abstract fun factAboutNumberDao(): FactAboutNumberDao
+
+    companion object {
+        const val DB_VERSION = 1
+    }
+
 }
