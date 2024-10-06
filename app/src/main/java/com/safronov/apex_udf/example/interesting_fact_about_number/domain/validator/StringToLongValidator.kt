b1d0263022e0820470d29c384a3f889c6dd3fce2
i-safronov <ilya.safa1337w@gmail.com>
2024-10-06 21:43:14 +0500

    Added StringToLongValidator.kt

---
+++ app/src/main/java/com/safronov/apex_udf/example/interesting_fact_about_number/domain/validator/StringToLongValidator.kt
@@ -0,0 +1,27 @@
+package com.safronov.apex_udf.example.interesting_fact_about_number.domain.validator
+
+import com.safronov.apex_udf.example.interesting_fact_about_number.domain.response.Response
+
+class StringToLongValidator {
+
+    fun validate(input: String): Response<Long> {
+        return when {
+            input.isBlank() -> {
+                Response.Error(error = Exception("Input text cannot be empty"))
+            }
+            input.all { it.isDigit() } -> {
+                Response.Success(data = input.toLong())
+            }
+            else -> {
+                Response.Error(error = Exception("Input must contain only digits"))
+            }
+        }
+    }
+}
+
+
+fun main() {
+    for (i in 0 until 3_147_483_647) {
+        println("$i")
+    }
+}
