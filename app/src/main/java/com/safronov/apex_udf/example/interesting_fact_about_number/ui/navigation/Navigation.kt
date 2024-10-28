41b62fdda221d3293a6867aa781b1bdb1166122f
i-safronov <ilya.safa1337w@gmail.com>
2024-10-28 16:21:23 +0500

    Added logic to get args in the Navigation.kt

--- app/src/main/java/com/safronov/apex_udf/example/interesting_fact_about_number/ui/navigation/Navigation.kt
+++ app/src/main/java/com/safronov/apex_udf/example/interesting_fact_about_number/ui/navigation/Navigation.kt
@@ -2,9 +2,13 @@ package com.safronov.apex_udf.example.interesting_fact_about_number.ui.navigatio
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.safronov.apex_udf.example.interesting_fact_about_number.ui.composable.number_details.NumberDetailsDestination
+import com.safronov.apex_udf.example.interesting_fact_about_number.ui.composable.number_details.NumberDetailsRoute
 import com.safronov.apex_udf.example.interesting_fact_about_number.ui.composable.numbers_list.NumbersListDestination
 import com.safronov.apex_udf.example.interesting_fact_about_number.ui.composable.numbers_list.NumbersListRoute
 
@@ -23,6 +27,30 @@ fun Navigation(
         ) {
             NumbersListDestination(
                 navController = navController
+            )
+        }
+
+        composable(
+            route = NumberDetailsRoute.path,
+            arguments = listOf(
+                navArgument(name = NumberDetailsRoute.Args.NUMBER.name) {
+                    type = NavType.StringType
+                    defaultValue = "0"
+                    nullable = false
+                },
+                navArgument(name = NumberDetailsRoute.Args.DESCRIPTION.name) {
+                    type = NavType.StringType
+                    defaultValue =
+                        "If a reference points to null, it simply means that there is no value associated with it.\n" +
+                                "\n" +
+                                "Technically speaking, the memory location assigned to the reference contains the value 0 (all bits at zero), or any other value that denotes null in the given environment."
+                    nullable = false
+                },
+            )
+        ) { entry ->
+            NumberDetailsDestination(
+                number = entry.arguments?.getString(NumberDetailsRoute.Args.NUMBER.name) ?: "",
+                description = entry.arguments?.getString(NumberDetailsRoute.Args.DESCRIPTION.name) ?: ""
             )
         }
     }
