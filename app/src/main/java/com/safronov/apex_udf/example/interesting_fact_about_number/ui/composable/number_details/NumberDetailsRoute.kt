a7d5397135141c70bff30ec72c90062c198dd336
i-safronov <ilya.safa1337w@gmail.com>
2024-10-28 16:13:33 +0500

    Added NumberDetailsRoute.kt

---
+++ app/src/main/java/com/safronov/apex_udf/example/interesting_fact_about_number/ui/composable/number_details/NumberDetailsRoute.kt
@@ -0,0 +1,37 @@
+package com.safronov.apex_udf.example.interesting_fact_about_number.ui.composable.number_details
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+
+class NumberDetailsRoute {
+    companion object {
+        val path = "number_details"
+    }
+}
+
+@Composable
+fun NumberDetailsDestination(
+    modifier: Modifier = Modifier,
+    number: String,
+    description: String
+) {
+    val viewModel: NumberDetailsViewModel = hiltViewModel()
+
+    LaunchedEffect(
+        number, description
+    ) {
+        viewModel.dispatch(
+            NumberDetailsContract.Executor.SetProperties(
+                number = number,
+                description = description
+            )
+        )
+    }
+
+    NumberDetailsScreen(
+        modifier = modifier,
+        state = viewModel.state
+    )
+}
