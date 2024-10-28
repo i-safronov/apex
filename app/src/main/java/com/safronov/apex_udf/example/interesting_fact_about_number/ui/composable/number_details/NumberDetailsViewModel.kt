1a797eff8f4fd080ac3d1d1ce476372d9743a6c1
i-safronov <ilya.safa1337w@gmail.com>
2024-10-28 16:08:21 +0500

    Implement NumberDetailsViewModel.kt

---
+++ app/src/main/java/com/safronov/apex_udf/example/interesting_fact_about_number/ui/composable/number_details/NumberDetailsViewModel.kt
@@ -0,0 +1,25 @@
+package com.safronov.apex_udf.example.interesting_fact_about_number.ui.composable.number_details
+
+import com.safronov.apex.udf.EffectorScope
+import com.safronov.apex.udf.ExecutorScope
+import com.safronov.apex.udf.UDFViewModel
+import com.safronov.apex_udf.example.interesting_fact_about_number.ui.composable.number_details.NumberDetailsContract.*
+
+class NumberDetailsViewModel : UDFViewModel<State, Executor, Effect, Event>(
+    initState = State()
+) {
+
+    override suspend fun ExecutorScope<Effect>.execute(ex: Executor): State = when(ex) {
+        is Executor.SetProperties -> {
+            state.copy(
+                number = ex.number,
+                description = ex.description
+            )
+        }
+    }
+
+    override suspend fun EffectorScope<Executor>.affect(ef: Effect) = when(ef) {
+        else -> {}
+    }
+
+}
